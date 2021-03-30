839b4cafc3068ac28d0926f64fc1dedbe2e4687b
jhajikha <75753980+jhajikha@users.noreply.github.com>
2021-03-30 00:04:52 -0700

    Update FlashSet.py

--- FlashSet.py
+++ FlashSet.py
@@ -146,17 +146,4 @@ class flashset():
                 print(f'You skipped {len(skipped)} card(s):\n')
                 
                 for item in skipped:
-                    print(f'{item}:{self.d[item]}\n')   
-            
-    
-        
-f=flashset({'ㅂ':'q', 'ㅈ':'w', 'ㄷ':'e', 'ㄱ':'r', 'ㅅ':'t', \
-              'ㅛ':'y', 'ㅕ':'u', 'ㅑ':'i', 'ㅐ':'o', 'ㅔ':'p', \
-              'ㅁ':'a', 'ㄴ':'s', 'ㅇ':'d', 'ㄹ':'f', 'ㅎ':'g', \
-              'ㅗ':'h', 'ㅓ':'j', 'ㅏ':'k', 'ㅣ':'l', 'ㅋ':'z', \
-              'ㅌ':'x', 'ㅊ':'c', 'ㅍ':'v', 'ㅠ':'b', 'ㅜ':'n', \
-              'ㅡ':'m', 'ㅃ':'Q', 'ㅉ':'W', 'ㄸ':'E', 'ㄲ':'R', \
-              'ㅆ':'T', 'ㅒ':'O', 'ㅖ':'P'})
-
-#f = flashset({'apple':'red','banana':'yellow','grapes':'green','orange':'orange', 'lemon':'yellow'})
-#f.test()
+                    print(f'{item}:{self.d[item]}\n')
